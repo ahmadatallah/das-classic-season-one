204a9ef502fc055d2dd4c0cdbf1f00a08d49beda
ahmadm-atallah <ahmad.atallah@yandex.com>
2018-06-28 10:42:44 +0200

    should, it, and describe defined

--- das-003/spec.rb
+++ das-003/spec.rb
@@ -7,7 +7,9 @@ class ExampleGroup
 		@block = block 
 	end
 	def evaluate!
-		@block.call
+		# once we have instance_eval into a method of class 
+		# this block will run as it was a method of that class(ExampleGroup)
+		instance_eval(&@block)
 	end
 
 	def it (description, &block)
@@ -15,4 +17,27 @@ class ExampleGroup
 	end 
 end
 
+class Object
+	def should(comparison_object=nil) 
+	  # to enable real rspec it is just an if condition 
+	  # if comparison_object
+          #  ...
+	  # else 
+	   DelayedAssertion.new(self)
+          #end
+	end
+end
+
+class AssertionError < Exception
+end
+
+class DelayedAssertion
+	def initialize(subject)
+		@subject = subject
+	end
+	def ==(other)
+		raise AssertionError unless @subject == other 
+	end
+
+end
 
--- das-003/test_spec.rb
+++ das-003/test_spec.rb
@@ -4,7 +4,7 @@ require './spec'
 class TestDescribe < Test::Unit::TestCase   
 	def test_that_it_can_pass 
 		#when I have nested blocks they dislock the environment chain of that class
-		#so the it method is not defined ,t
+		#so the it method is not defined
 		describe 'some thing' do
 		   it 'has some property' do 
 		   end	   
@@ -13,12 +13,23 @@ class TestDescribe < Test::Unit::TestCase
 	def test_it_can_fail 
 		assert_raise(IndexError)do
 			describe 'some failing' do
-		 	it 'fails' do
-			     raise IndexError
-              		  end
-                	end
+		 		it 'fails' do
+	               	 		raise IndexError
+				end
 	
+			end
+		end
+	end	
+end
+
+class TestAssertion < Test::Unit::TestCase
+	def test_that_it_can_pass 
+		2.should == 2
+	end
+	def test_that_it_can_fail 
+		assert_raise(AssertionError) do 
+		1.should == (2) 
 		end
 	end
-	
+
 end
