aaaf84ea03cec0adebf1f0003a9ccafc9942f54a
Ahmad Atallah <ahmad.atallah@yandex.com>
2018-12-22 16:55:20 +0200

    replace tab indentation with 3 spaces

--- das-002/avoid_nil_1.rb
+++ das-002/avoid_nil_1.rb
@@ -1,28 +1,28 @@
 class Person 
-	attr_reader :name 
-	def initialize(name) 
-		@name = name
-	end 
-
-	def self.find(id) 
-		people = {1 => new('alice'), 2 => new('bob')} 
-		people.fetch(id) 
-	end 
+  attr_reader :name 
+  def initialize(name) 
+     @name = name
+  end 
+	
+  def self.find(id) 
+     people = {1 => new('alice'), 2 => new('bob')} 
+     people.fetch(id) 
+  end 
 end 
 
 class SubscriptionsController 
-	def create(person_id)
-		person = Person.find(person_id)
-		Subscription.create_for_person(person)
-	end 
+   def create(person_id)
+      person = Person.find(person_id)
+      Subscription.create_for_person(person)
+   end 
 end 
 
 class Subscription
-	def self.create_for_person(person)
-	create!(:person => person, :person_name => person.name)
-	end
-        def self.create!(*args)
-      	end 	       
+   def self.create_for_person(person)
+      create!(:person => person, :person_name => person.name)
+   end
+   def self.create!(*args)
+   end 	       
 end
 
 SubscriptionsController.new.create(3) 
